ce7a55e395eb8859f2354a30bb2c654d6ce7db9b
MehediHasan128 <mehedihasan12926@gmail.com>
2024-06-15 00:26:02 +0600

    Add Not found functionlity

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import router from './app/routes';
+import NotFound from './app/middlwares/NotFound';
 const app: Application = express();
 
 app.use(express.json());
@@ -11,5 +12,7 @@ app.use('/api', router);
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use(NotFound);
 
 export default app;
